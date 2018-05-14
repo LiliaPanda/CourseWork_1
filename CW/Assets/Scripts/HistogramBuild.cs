e077a9837b6c9b88d4c642df6af0bba428426eb8
LiliaPanda <liliapandav@gmail.com>
2018-05-14 09:08:48 +0300

    Finished histo
    + Finished logics
    + Finished histo drawing
    - Axis

--- CW/Assets/Scripts/HistogramBuild.cs
+++ CW/Assets/Scripts/HistogramBuild.cs
@@ -31,6 +31,7 @@ public class HistogramBuild : MonoBehaviour {
 	private double[] statesAmplitudes;
 
 	int nQubits;
+	int curOpSize;
 
 	string path = "";
 
@@ -52,39 +53,68 @@ public class HistogramBuild : MonoBehaviour {
 	private bool CalculateOperation(MyOperator o){
 		if (!IsValidOperator (o.matrix))
 			return false;
-		print (statesAmplitudes.Length);
-		print (o.size);
-		for (int a = 0; a < statesAmplitudes.Length; a++) {					
+		int qsize = o.qubits.Length;
+		if (Math.Pow (2, qsize) != o.size)
+			return false;
+		curOpSize = o.size;
+		for (int a = 0; a < statesAmplitudes.Length; a++) {	
+			string res = "";
+			for (int i = 0; i < statesAmplitudes.Length; i++)
+				res = res + " " + statesAmplitudes [i];
+			print (res+" first "+a);
 			// choose only nessasary bits from state for calculation
-			int[] bits = Dec2Bin(a);
-			int[] nbits;
-			for (int i = 0; i < o.qubits.Length; i++) {
-				nbits [i] = bits [o.qubits [i]];
+			int[] bits = new Int32[nQubits];
+			Dec2Bin (a, ref bits);
+
+			int[] nbits = new Int32[qsize]; //nbits length - qsize
+			for (int i = 0; i < qsize; i++) {
+				nbits[i] = bits [o.qubits [i]];
 			}
+
 			// transform state to vector
-			int k = Bin2Dec(nbits);
 			double[] vector = new Double[o.size];
-			Array.Clear (vector, 0.0, o.size);
+			for (int i = 0; i < o.size; i++) {
+				vector [i] = 0.0;
+			}
+			int k = 0;
+			Bin2Dec (ref k, nbits);
 			vector [k] = 1.0;
 
 			// apply operator
 			vector = MultMatrixVector(o.matrix, vector);
+
 			// change them
 			for (int i = 0; i < o.size; i++){
+				// if vector [i] != 0, so binary decomposition of i need to be setup for some places in bits
 				if (vector [i] > 0.00001 || vector[i] < -0.00001) {
-					
+					Dec2Bin (i, ref nbits);
+
+					// change bits in decomposition
+					for (int j = 0; j < qsize; j++) {
+						bits[o.qubits [j]] = nbits[j];
+					}
+
+					// set up amplitudes
+					Bin2Dec (ref k, bits);
+					statesAmplitudes [k] += vector[i];
 				}
 			}
-			// set up amplitude for state we got after permutation
+			res = "";
+			for (int i = 0; i < statesAmplitudes.Length; i++)
+				res = res + " " + statesAmplitudes [i];
+			print (res+" second "+a);
 		}
 		return true; 
 	}
 
 	// validate operator matrix
 	private bool IsValidOperator(double[][] m){
-		double[][] mres;
-		int size1 = m.GetUpperBound (0);
-		int size2 = m.GetUpperBound (1);
+		int size1 = m.Length;
+		int size2 = m[0].Length;
+		double[][] mres = new Double[size1] [];
+		for (int i = 0; i < size1; i++) {
+			mres [i] = new Double[size2];
+		}
 		if (size1 != size2)
 			return false;
 		
@@ -94,63 +124,70 @@ public class HistogramBuild : MonoBehaviour {
 				mres [i] [j] = MultVectorVector (mres [i], mres [j]);
 			}
 		}
-		double[][] ematrix;
-		Array.Clear (ematrix, 0.0, size1);
-		for (int i = 0; i < size1; i++) {
-			ematrix [i] [i] = 1.0;
-		}
 
-		for (int i = 0; i < size1; i++){
+		for (int i = 0; i < size1; i++) {
 			for (int j = 0; j < size2; j++) {
-				if (mres [i] [j] - ematrix [i] [j] < 0.00001)
+				if ((i == j) && (mres [i] [j] - 1 > 0.00001))
+					return false;
+				if ((i != j) && (mres [i] [j] > 0.00001))
 					return false;
 			}
 		}
+
 		return true;
 	}
 
 	// permutate multiply matrix with vector
-	private bool MultMatrixVector(double[][] m, double[] v){
+	private double[] MultMatrixVector(double[][] m, double[] v){
+		double[] res = new Double[m[0].Length];
 		if (m.Length != v.Length)
-			return false;
-
-		return true;
-	}
-
-	// permutate multiply matrix with matrix
-	private bool MultMatrixMatrix(double[][] m1, double[][] m2){
-		if (m1.Length != m2.Length)
-			return false;
-		for (int i = 0; i < m1.Length; 
-		)
-			return true;
+			return res;
+		
+		for (int i = 0; i < m.Length; i++) {
+			res [i] = 0;
+			for (int j = 0; j < v.Length; j++) {
+				res [i] += m [i] [j] * v [j];
+			} 
+		}
+		return res;
 	}
 
 	// permutate multiply vector with vector
 	private double MultVectorVector(double[] v1, double[] v2){
 		double res = 0;
+		if (v1.Length != v2.Length)
+			return res;
 		for (int i = 0; i < v1.Length; i++) {
 			res += v1 [i] * v2 [i];
 		} 
 		return res;
 	}
 
-	private int[] Dec2Bin(int a){
-		int[] bits;
-		for (int i = 0; a > 0; i++) {
+	// permutate multiply matrix with matrix
+	/*private bool MultMatrixMatrix(double[][] m1, double[][] m2){
+		if (m1.Length != m2.Length)
+			return false;
+		for (int i = 0; i < m1.Length; 
+		)
+			return true;
+	}*/
+
+	private void Dec2Bin(int a, ref int[] bits){
+		for (int i = 0; i < bits.Length; i++) {
+			bits [i] = 0;
+		}
+		for (int i = bits.Length-1; a > 0; i--) {
 			bits [i] = a % 2;
 			a /= 2;
 		}
-		return bits;
 	}
 
-	private int Bin2Dec(int[] bits){
-		int a = 0;
+	private void Bin2Dec(ref int a, int[] bits){
+		a = 0;
 		int pow = 0;
-		for (int i = 0; i < bits.Length; i++ & pow++) {
-			a += bits [i] * pow (2, pow);
+		for (int i = bits.Length-1; i >= 0; i-- , pow++) {
+			a += bits [i] * (int)Math.Pow (2, pow);
 		}
-		return a;
 	}
 
 	private void GenerateHisto(){
@@ -160,8 +197,6 @@ public class HistogramBuild : MonoBehaviour {
 		int nStates = (int)Math.Pow(2,nQubits);
 
 		statesAmplitudes = new double[nStates];
-		for (int i = 0; i < nStates; i++)
-			statesAmplitudes [i] = new double();
 
 		foreach (MyOperator o in operatorsList) {
 			CalculateOperation (o);
@@ -170,31 +205,34 @@ public class HistogramBuild : MonoBehaviour {
 			mesh.SetVertices(Vertices);
 			mesh.SetTriangles (Triangles,0);
 			mesh.RecalculateNormals ();
+			mFilter.mesh.Clear ();
 			mFilter.mesh = mesh;
 
-			System.Threading.Thread.Sleep(10000);
+			System.Threading.Thread.Sleep(2000);
 		}
 
 		mesh.SetVertices(Vertices);
 		mesh.SetTriangles (Triangles,0);
 		mesh.RecalculateNormals ();
 
+		mFilter.mesh.Clear ();
 		mFilter.mesh = mesh;
 	}
 
 	private void GenerateHistoState(){
-		float expand = (float)(width / (Math.Pow (2, nQubits)));
+		float expand = (float)(width / 1920);
 		float shift = 10;
-		size = 100;
+		float colWidth = 200;
+		float colHeight = 500;
 		Vertices = new List<Vector3> ();
 		Triangles = new List<int> ();
 
 		for (int a = 0; a < statesAmplitudes.Length; a++) {
 			//Real column
-			Vertices.Add (new Vector3 ((float) a * expand * size + shift, 0.0f, 0.0f));//left bottom
-			Vertices.Add (new Vector3 ((float)(a+1) * expand * size, 0.0f, 0.0f));//right bottom
-			Vertices.Add (new Vector3 ((float)(a+1) * expand * size + shift, statesAmplitudes[a]*size, 0.0f));//right top
-			Vertices.Add (new Vector3 ((float) a * expand * size, statesAmplitudes[a]*size, 0.0f));//right top
+			Vertices.Add (new Vector3 ((float) a * (shift + colWidth) * expand + shift , 0.0f, 0.0f));//left bottom
+			Vertices.Add (new Vector3 ((float) a * (shift + colWidth) * expand + shift, (float)statesAmplitudes[a]*colHeight, 0.0f));//right bottom
+			Vertices.Add (new Vector3 ((float) (a+1) * (shift + colWidth) * expand, (float)statesAmplitudes[a]*colHeight, 0.0f));//right top
+			Vertices.Add (new Vector3 ((float) (a+1) * (shift + colWidth) * expand, 0.0f, 0.0f));//right top
 
 			Triangles.Add (0+4*a);
 			Triangles.Add (1+4*a);
@@ -226,34 +264,31 @@ public class HistogramBuild : MonoBehaviour {
 				throw new UnityException("Too much qubits");
 			}
 			// let each line to be a permutation - string <name> : <arg1>, <arg2>, <arg3>, ...
-			for (int p = 1; p < Algo.Length; ) {
+			for (int op = 1; op < Algo.Length; ) {
 				MyOperator mOperator = new MyOperator();
 
-				mOperator.size = int.Parse(Algo[p]);
-				p++;
-
-				double[,] a = new double[mOperator.size,mOperator.size];
-				mOperator.matrix = new double[mOperator.size,mOperator.size];
+				// parse operator matrix size
+				mOperator.size = int.Parse(Algo[op]);
+				op++;
+				// parse operator matrix
+				mOperator.matrix = new double[mOperator.size][];
 				for (int i = 0; i < mOperator.size; i++){
-					string[] COperator = Algo[p+i].Split(new char[]{' '}, StringSplitOptions.RemoveEmptyEntries);
-
-					for (int j = 0; j < mOperator.size; j++){
-						string[] complexOp = COperator[j].Split(new char[]{','}, StringSplitOptions.RemoveEmptyEntries);
-						//print (Single.Parse(complexOp[0])+1+" "+Single.Parse(complexOp[1]));
-						mOperator.matrix[i,j] = new double(Single.Parse(complexOp[0]),Single.Parse(complexOp[1]));
-						//mOperator.matrix[i,j] = new double(0.0f,1.0f);
+					mOperator.matrix[i] = new Double[mOperator.size];
+					string[] Operator = Algo[op+i].Split(new char[]{' '}, StringSplitOptions.RemoveEmptyEntries);
+					for (int j = 0; j < Operator.Length; j++){
+						mOperator.matrix[i][j] = Single.Parse(Operator[j]);
 					}
 				}
-				p += mOperator.size;
+				op += mOperator.size;
 
-				string[] Arguments = Algo[p].Split(new char[]{' '}, StringSplitOptions.RemoveEmptyEntries);
+				string[] Arguments = Algo[op].Split(new char[]{','}, StringSplitOptions.RemoveEmptyEntries);
 				mOperator.qubits = new int[Arguments.Length];
 				for (int i = 0; i < Arguments.Length; i++){
 					mOperator.qubits[i] = int.Parse(Arguments[i]);
 				}
 
 				operatorsList.Add(mOperator);
-				p++;
+				op++;
 			}
 		}
 		catch (Exception e) {
