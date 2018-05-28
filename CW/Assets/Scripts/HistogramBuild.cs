359fb8b765a137cd1f90c92a58579769cd43b1b8
LiliaPanda <liliapandav@gmail.com>
2018-05-28 11:45:48 +0300

    Final
    - Finished all program
    - Fixed histo drawing
    - Fixed operator calculation

--- CW/Assets/Scripts/HistogramBuild.cs
+++ CW/Assets/Scripts/HistogramBuild.cs
@@ -2,9 +2,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
-using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Linq;
 
 [RequireComponent(typeof(MeshFilter))]
 public class HistogramBuild : MonoBehaviour {
@@ -12,10 +12,13 @@ public class HistogramBuild : MonoBehaviour {
 	public int size = 100;
 
 	int MAX_QUBITS = 6;
-
+	int nextOperator = 0;
 	private Text message;
 
 	private MeshFilter mFilter;
+
+	private List<Mesh> meshes;
+	private Mesh startMesh;
 
 	List<Vector3> Vertices;
 	List<int> Triangles;
@@ -44,9 +47,22 @@ public class HistogramBuild : MonoBehaviour {
 		//mFilter.mesh = GenerateMesh ();
 	}
 
+	void Update(){
+		
+	}
+
 	public void ProceedHisto(){
-		if (AddAlgoFile () && ReadAlgoFromFile ())
+		if (ReadAlgoFromFile ()) {
 			GenerateHisto ();
+			nextOperator = 0;
+		}
+	}
+
+	public void ShowMyMesh(){
+		mFilter.mesh.Clear ();
+		mFilter.mesh = meshes.ElementAt (nextOperator);
+		//System.Threading.Thread.Sleep (5000);
+		nextOperator++;
 	}
 
 	// permutate operation
@@ -57,11 +73,19 @@ public class HistogramBuild : MonoBehaviour {
 		if (Math.Pow (2, qsize) != o.size)
 			return false;
 		curOpSize = o.size;
+
+		double[] newAmplitudes = new Double[(int)Math.Pow (2, nQubits)];
+
+		for (int i = 0; i < (int)Math.Pow (2, nQubits); i++) {
+			newAmplitudes [i] = 0;
+		}
 		for (int a = 0; a < statesAmplitudes.Length; a++) {	
+			double ampl = statesAmplitudes [a];
 			string res = "";
-			for (int i = 0; i < statesAmplitudes.Length; i++)
-				res = res + " " + statesAmplitudes [i];
-			print (res+" first "+a);
+			for (int i = 0; i < statesAmplitudes.Length; i++) res = res + " " + statesAmplitudes [i];
+			print (res + " first " + a);
+			print (ampl);
+
 			// choose only nessasary bits from state for calculation
 			int[] bits = new Int32[nQubits];
 			Dec2Bin (a, ref bits);
@@ -96,14 +120,14 @@ public class HistogramBuild : MonoBehaviour {
 
 					// set up amplitudes
 					Bin2Dec (ref k, bits);
-					statesAmplitudes [k] += vector[i];
+					newAmplitudes [k] += vector[i]*ampl;
 				}
 			}
 			res = "";
-			for (int i = 0; i < statesAmplitudes.Length; i++)
-				res = res + " " + statesAmplitudes [i];
+			for (int i = 0; i < statesAmplitudes.Length; i++) res = res + " " + newAmplitudes [i];
 			print (res+" second "+a);
 		}
+		statesAmplitudes = newAmplitudes;
 		return true; 
 	}
 
@@ -132,8 +156,7 @@ public class HistogramBuild : MonoBehaviour {
 				if ((i != j) && (mres [i] [j] > 0.00001))
 					return false;
 			}
-		}
-
+		}	
 		return true;
 	}
 
@@ -191,108 +214,129 @@ public class HistogramBuild : MonoBehaviour {
 	}
 
 	private void GenerateHisto(){
-		mFilter  = GetComponent<MeshFilter> ();
+		meshes = new List<Mesh> ();
+		mFilter = GetComponent<MeshFilter> ();
 		Mesh mesh = new Mesh ();
 
-		int nStates = (int)Math.Pow(2,nQubits);
+		int nStates = (int)Math.Pow (2, nQubits);
+	
+		GenerateHistoState ();
 
-		statesAmplitudes = new double[nStates];
+		mesh.SetVertices (Vertices);
+		mesh.SetTriangles (Triangles, 0);
+		mesh.RecalculateNormals ();
+
+		meshes.Add (mesh);
 
 		foreach (MyOperator o in operatorsList) {
+			Mesh mesh2 = new Mesh ();
 			CalculateOperation (o);
 			GenerateHistoState ();
 
-			mesh.SetVertices(Vertices);
-			mesh.SetTriangles (Triangles,0);
-			mesh.RecalculateNormals ();
-			mFilter.mesh.Clear ();
-			mFilter.mesh = mesh;
+			mesh2.SetVertices (Vertices);
+			mesh2.SetTriangles (Triangles, 0);
+			mesh2.RecalculateNormals ();
 
-			System.Threading.Thread.Sleep(2000);
+			meshes.Add (mesh2);
 		}
-
-		mesh.SetVertices(Vertices);
-		mesh.SetTriangles (Triangles,0);
-		mesh.RecalculateNormals ();
-
-		mFilter.mesh.Clear ();
-		mFilter.mesh = mesh;
 	}
 
 	private void GenerateHistoState(){
-		float expand = (float)(width / 1920);
-		float shift = 10;
-		float colWidth = 200;
-		float colHeight = 500;
+		float resize_x = (float)(width / 1920);
+		float resize_y = (float)(width / 1080);
+		float shift = 1;
+		float colWidth = 40;
+		float colHeight = 280;
 		Vertices = new List<Vector3> ();
 		Triangles = new List<int> ();
 
 		for (int a = 0; a < statesAmplitudes.Length; a++) {
-			//Real column
-			Vertices.Add (new Vector3 ((float) a * (shift + colWidth) * expand + shift , 0.0f, 0.0f));//left bottom
-			Vertices.Add (new Vector3 ((float) a * (shift + colWidth) * expand + shift, (float)statesAmplitudes[a]*colHeight, 0.0f));//right bottom
-			Vertices.Add (new Vector3 ((float) (a+1) * (shift + colWidth) * expand, (float)statesAmplitudes[a]*colHeight, 0.0f));//right top
-			Vertices.Add (new Vector3 ((float) (a+1) * (shift + colWidth) * expand, 0.0f, 0.0f));//right top
 
-			Triangles.Add (0+4*a);
-			Triangles.Add (1+4*a);
-			Triangles.Add (2+4*a);
+			if (statesAmplitudes [a] > 0.00001) {
+				Vertices.Add (new Vector3 ((float)a * (shift + colWidth) * resize_x + shift, 0.0f, 0.0f));//left bottom
+				Vertices.Add (new Vector3 ((float)a * (shift + colWidth) * resize_x + shift, (float)statesAmplitudes [a] * colHeight * resize_y, 0.0f));//right bottom
+				Vertices.Add (new Vector3 ((float)(a + 1) * (shift + colWidth) * resize_x, (float)statesAmplitudes [a] * colHeight * resize_y, 0.0f));//right top
+				Vertices.Add (new Vector3 ((float)(a + 1) * (shift + colWidth) * resize_x, 0.0f, 0.0f));//right top
+			} else if (statesAmplitudes [a] < 0.00001) {
+				Vertices.Add (new Vector3 ((float)a * (shift + colWidth) * resize_x + shift, (float)statesAmplitudes [a] * colHeight * resize_y, 0.0f));//right bottom
+				Vertices.Add (new Vector3 ((float)a * (shift + colWidth) * resize_x + shift, 0.0f, 0.0f));//left bottom
+				Vertices.Add (new Vector3 ((float)(a + 1) * (shift + colWidth) * resize_x, 0.0f, 0.0f));//right top
+				Vertices.Add (new Vector3 ((float)(a + 1) * (shift + colWidth) * resize_x, (float)statesAmplitudes [a] * colHeight * resize_y, 0.0f));//right top
+			} else {
+				Vertices.Add (new Vector3 ((float)a * (shift + colWidth) * resize_x + shift, 0.0f, 0.0f));//left bottom
+				Vertices.Add (new Vector3 ((float)a * (shift + colWidth) * resize_x + shift, (float)statesAmplitudes [a] * 2, 0.0f));//right bottom
+				Vertices.Add (new Vector3 ((float)(a + 1) * (shift + colWidth) * resize_x, (float)statesAmplitudes [a] * 2, 0.0f));//right top
+				Vertices.Add (new Vector3 ((float)(a + 1) * (shift + colWidth) * resize_x, 0.0f, 0.0f));//right top
+			}
+			Triangles.Add (0 + 4 * a);
+			Triangles.Add (1 + 4 * a);
+			Triangles.Add (2 + 4 * a);
 
-			Triangles.Add (0+4*a);
-			Triangles.Add (2+4*a);
-			Triangles.Add (3+4*a);
+			Triangles.Add (0 + 4 * a);
+			Triangles.Add (2 + 4 * a);
+			Triangles.Add (3 + 4 * a);
 		}
 	}
 
-	private bool AddAlgoFile(){
+	/*private bool AddAlgoFile(){
 		path = EditorUtility.OpenFilePanel("Choose algorithm description", "", "txt");
 		if (path != "")
 			return true;
 		return false;
 	}
-
+*/
 	private bool ReadAlgoFromFile(){
 		try { 
 			//Read operator's matrix size, read matrix
+			path = "test.txt";
+			int op = 0;
 			operatorsList = new List<MyOperator> ();
-			string[] Algo = System.IO.File.ReadAllLines(path);
+			string[] Algo = System.IO.File.ReadAllLines (path);
 
 			// file need to contain qubit quantity as first line!
-			nQubits = int.Parse(Algo[0]);
-			if (nQubits > MAX_QUBITS){
+			nQubits = int.Parse (Algo [op]);
+			if (nQubits > MAX_QUBITS) {
 				message.text = "Too much qubits! No more than " + MAX_QUBITS + " is allowed!";
-				throw new UnityException("Too much qubits");
+				throw new UnityException ("Too much qubits");
 			}
-			// let each line to be a permutation - string <name> : <arg1>, <arg2>, <arg3>, ...
-			for (int op = 1; op < Algo.Length; ) {
-				MyOperator mOperator = new MyOperator();
+			op++;
+			// read start amplitudes
+			string stateString = Algo [op];
+			string[] states = stateString.Split (new char[]{ ' ' }, StringSplitOptions.RemoveEmptyEntries);
+			statesAmplitudes = new Double[(int)Math.Pow (2, nQubits)];
+			for (int i = 0; i < (int)Math.Pow (2, nQubits); i++) {
+				statesAmplitudes [i] = Single.Parse (states [i]);
+			}
+			op++;
+			for (; op < Algo.Length;) {
+				MyOperator mOperator = new MyOperator ();
 
 				// parse operator matrix size
-				mOperator.size = int.Parse(Algo[op]);
+				mOperator.size = int.Parse (Algo [op]);
 				op++;
 				// parse operator matrix
 				mOperator.matrix = new double[mOperator.size][];
-				for (int i = 0; i < mOperator.size; i++){
-					mOperator.matrix[i] = new Double[mOperator.size];
-					string[] Operator = Algo[op+i].Split(new char[]{' '}, StringSplitOptions.RemoveEmptyEntries);
-					for (int j = 0; j < Operator.Length; j++){
-						mOperator.matrix[i][j] = Single.Parse(Operator[j]);
+				for (int i = 0; i < mOperator.size; i++) {
+					mOperator.matrix [i] = new Double[mOperator.size];
+					string[] Operator = Algo [op + i].Split (new char[]{ ' ' }, StringSplitOptions.RemoveEmptyEntries);
+					for (int j = 0; j < Operator.Length; j++) {
+						mOperator.matrix [i] [j] = Single.Parse (Operator [j]);
 					}
 				}
 				op += mOperator.size;
 
-				string[] Arguments = Algo[op].Split(new char[]{','}, StringSplitOptions.RemoveEmptyEntries);
+				string[] Arguments = Algo [op].Split (new char[]{ ',' }, StringSplitOptions.RemoveEmptyEntries);
 				mOperator.qubits = new int[Arguments.Length];
-				for (int i = 0; i < Arguments.Length; i++){
-					mOperator.qubits[i] = int.Parse(Arguments[i]);
+				for (int i = 0; i < Arguments.Length; i++) {
+					mOperator.qubits [i] = int.Parse (Arguments [i]);
 				}
 
-				operatorsList.Add(mOperator);
+				operatorsList.Add (mOperator);
 				op++;
 			}
-		}
-		catch (Exception e) {
+		} catch (Exception e) {
 			print (e.Message);
+			print ("ваще не ок");
 			return false;
 		}
 		return true;
